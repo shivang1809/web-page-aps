7a1a523133295b73e6ccc037f9cbe17abed6ff54
Shivang <83906239+shivang1809@users.noreply.github.com>
2022-04-20 18:23:45 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,16 @@
+function navFunction(){
+    var x = document.getElementById("linkbox");
+    var y = document.getElementById("bar");
+    var z = document.getElementById("bar2");
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "block";
+};
+function navFunction1(){
+    var x = document.getElementById("linkbox");
+    var y = document.getElementById("bar");
+    var z = document.getElementById("bar2");
+    x.style.display = "none";
+    y.style.display = "block"
+    z.style.display = "none";
+};
